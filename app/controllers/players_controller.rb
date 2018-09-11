23509947de8ae69b2cfbc9e24fdf575a1f69a03a
Gabriel <gabrielgomesch@gmail.com>
2018-09-11 07:41:04 -0300

    feature(player): controller and route for player's crud

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,43 @@
+class PlayersController < ApplicationController
+  before_action :find_player, only: [:show, :update, :destroy]
+
+  # GET /players
+  def index
+    @players = Player.all
+    json_response(@players)
+  end
+
+  # POST /players
+  def create
+    @player = Player.create!(player_params)
+    json_response(@player, :created)
+  end
+
+  # GET /players/:id
+  def show
+    json_response(@player)
+  end
+
+  # PUT /players/:id
+  def update
+    @player.update(player_params)
+    # head :no_content
+  end
+
+  # DELETE /players/:id
+  def destroy
+    @player.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_player
+    @player = Player.find(params[:id])
+  end
+
+  def player_params
+    # whitelist params
+    params.permit(:name)
+  end
+end
